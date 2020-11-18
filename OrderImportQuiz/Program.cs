3c272295bcf89ddcd3d6d8bb43a48bfe886ee258
EliasJany <eliasj.n.2117@gmail.com>
2020-11-18 12:27:54 +0100

    Grunddaten angelegt

--- OrderImportQuiz/Program.cs
+++ OrderImportQuiz/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
-namespace OrderImportQuiz
+Console.WriteLine("Hello World");
+
+class Customer
 {
-    class Program
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [Column(TypeName = "decimal(8,2)")]
+    public decimal CreditLimit { get; set; }
+}
+
+class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime OrderDate { get; set; }
+    [Column(TypeName = "decimal(8,2)")]
+    public decimal OrderValue { get; set; }
+}
+
+class QuizContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public QuizContext(DbContextOptions<QuizContext> options) : base(options) { }
+}
+
+class QuizContextFactory : IDesignTimeDbContextFactory<QuizContext>
+{
+    public QuizContext CreateDbContext(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<QuizContext>();
+
+        optionsBuilder
+            //.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+            .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        return new QuizContext(optionsBuilder.Options);
     }
 }
