3aca42d680121f737585901fef47ffaf776de40f
EliasJany <eliasj.n.2117@gmail.com>
2020-11-18 12:41:05 +0100

    .

--- OrderImportQuiz/Program.cs
+++ OrderImportQuiz/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.Linq;
 
 Console.WriteLine("Hello World");
